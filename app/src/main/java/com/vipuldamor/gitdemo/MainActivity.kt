cb02f9aeee4c69571dad20e20839fafdcd52afe9
vipul <vipuldamor@gmail.com>
2025-02-24 11:36:12 +0530

    first commit

--- app/src/main/java/com/vipuldamor/gitdemo/MainActivity.kt
+++ app/src/main/java/com/vipuldamor/gitdemo/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        //this is my test code
         setContent {
             GitdemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
